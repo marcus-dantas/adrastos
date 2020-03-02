055adc4a6b8ef38b12ea80260fa4353c01e63117
Marcus Dantas <marcus@dantas.co.uk>
2020-03-02 17:22:17 +0000

    Created article controller with methods

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,7 +1,48 @@
 require 'test_helper'
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  before_action :find_article, only: [ :show, :edit, :update, :destroy ]
+  def index
+    @articles = Article.all
+  end
+
+  def show; end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.user = current_user
+    if @article.save
+      flash[:notice] = "Article Successfully created"
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    @article.update(params[:article])
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:content, :user, :title)
+  end
+
 end
