5a23001d3e42b69f3aa076dd9e628ff3fd948c53
Kaloyan Atansov <kaloyanatanasov83@gmail.com>
2020-03-10 15:08:07 +0000

    Added  mentions in the comment page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    include ActionText::Attachable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -8,4 +9,7 @@ class User < ApplicationRecord
   has_many :articles, dependent: :destroy
   has_many :discussions, through: :articles
   has_one_attached :avatar
+  def to_trix_content_attachment_patial_path
+    to_partial_path
+  end
 end
